bc8ed3a0b1dcb83b8554d609e05cd76495c3bb12
OMEGA <y80741701@gmail.com>
2024-11-14 15:46:32 +0300

    Test1

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,9 +1,2 @@
-import datetime
-print(datetime.datetime)
-print('! Hello!')
-while True:
-    string = input('> Enter what to calculate: ').strip().lower()
-    if string == 'q':
-        print('! Bye!')
-        break
-    print(eval(string))
+import os
+print(os.path)
